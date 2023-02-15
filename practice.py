e894819a7e21d4afa97ce463c13945367d93757e
Hamsss <gka304@gmail.com>
2023-02-15 14:16:34 +0900

    2023-02-15

--- practice.py
+++ practice.py
@@ -1,8 +1,28 @@
-print("hello world")
+first = 1
+second = 1
 
-print("hey")
+sum = 0
+line = 1
+count = 0
+what = int(input())
 
-a = 1
-b = 2
-c = a + b
-print(c)
+while(1):
+    sum += line
+    if (sum >= what):
+        count = what - (sum - line)
+        break
+    line += 1
+
+if line%2==0:
+    second = line
+    for i in range(count-1):
+        second -= 1
+        first += 1
+else:
+    first = line
+    for i in range(count-1):
+        first -= 1
+        second += 1
+
+print(f"{first}/{second}")
+
